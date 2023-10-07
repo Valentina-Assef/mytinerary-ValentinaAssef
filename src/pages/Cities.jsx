a3a578b981cfff5941d31a5a317ff74c883da66e
Valentina-Assef <valenassef@gmail.com>
2023-10-06 21:49:48 -0300

    Cities page v2.0

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -24,21 +24,37 @@ export default function Cities() {
 
   return (
     <>
-      <header className='flex justify-center items-center relative -z-10'>
+      <header className='flex justify-center items-center text-center'>
         <img className='h-[70vh] w-screen object-cover' src={img} alt='Img Header'/>
         <h2 className="text-white text-7xl p-2 font-text absolute">Find your perfect trip</h2>
+        <div className="search-bar w-[70%] md:w-[50%] lg:w-[25%] mt-64 md:mt-56 rounded-full absolute bg-white py-0.5 pr-0.5 pl-2.5">
+          <form role="search" className="flex items-center justify-between pl-5">
+            <input ref={inputSearch} id="searchInput" type="text" placeholder="Search" aria-label="Search" className="w-[50%] outline-none"/>
+            <button onClick={handleSearch} aria-label="Search" type="button" className="bg-red-500 w-[45px] h-[45px] rounded-full outline-none">
+              <ion-icon name="search-outline" style={{ fontSize: "1.5rem", color: "#fff"}}></ion-icon>
+            </button>
+          </form>
+        </div>
+        
+        {/* <form className="mt-64 absolute flex justify-center items-center" role="search">
+          <input ref={inputSearch} id="searchInput" className="border-2 rounded p-2" type="text" placeholder="Search" aria-label="Search" />
+          <button onClick={handleSearch} aria-label="Search" type="button">
+            <ion-icon name="search-outline" style={{ fontSize: "2rem", color: "#fff", marginLeft: "1rem"}}></ion-icon>
+          </button>
+        </form> */}
       </header>
-      <section className="m-3"> 
+      <section className="flex justify-center items-center p-8">
+        <Gallery cities={cities}/>
+      </section>
+    </>
+  )
+}
+
+{/* <section className="m-3"> 
         <form className="flex justify-center items-center" role="search">
           <input ref={inputSearch} id="searchInput" className="border-2 border-amber-500 rounded mx-2 p-2 focus:border-amber-500 focus:outline-none" type="text" placeholder="Search" aria-label="Search" />
           <button onClick={handleSearch} aria-label="Search" type="button">
             <ion-icon name="search-outline" style={{ fontSize: "2rem", color: "#FFC107", position: "relative", zIndex: "-10"}}></ion-icon>
           </button>
         </form>
-      </section>
-      <section className="flex justify-center items-center bg-amber-500 p-10">
-        <Gallery cities={cities}/>
-      </section>
-    </>
-  )
-}
+      </section> */}
