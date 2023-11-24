e309549fabec18889b260978262c83dab10ceb9e
Valentina-Assef <valenassef@gmail.com>
2023-11-24 01:46:02 -0300

    Update Forms

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,11 +4,11 @@ import { Link as LinkRouter } from 'react-router-dom'
 export default function SignIn() {
   return (
     <div className="img_signIn flex justify-center items-center text-center">
-      <div className="flex flex-wrap rounded-lg bg-white w-[678px] max-w-[75vw] md:max-w-[90vw] md:min-h-[400px] md:max-h-[400px] mt-32 md:mt-24 mb-10 form-shadow">
-        <div className="bg-white md:w-1/2 w-full px-3 md:px-5 rounded-lg">
+      <div className="border-gray-200 border-2 flex flex-wrap rounded-lg bg-white w-[678px] max-w-[75vw] md:max-w-[90vw] md:min-h-[400px] md:max-h-[400px] mt-32 md:mt-24 mb-10 form-shadow">
+        <div className="bg-white  md:w-1/2 w-full px-3 md:px-5 rounded-lg">
           <FormSignIn />
         </div>
-        <div className="bg-red-500 md:w-1/2 w-full flex flex-col justify-center items-center p-5 rounded-b-lg md:rounded-bl-none md:rounded-tr-lg md:rounded-br-lg">
+        <div className="bg-red-500 text-white md:w-1/2 w-full flex flex-col justify-center items-center p-5 rounded-b-lg md:rounded-bl-none md:rounded-tr-lg md:rounded-br-lg">
           <h2 className="text-center text-2xl font-bold sm:text-3xl">No account?</h2>
           <p className="my-8">Enter Your personal details and start journey with us</p>
           <button type="submit" className="rounded-3xl border-2 border-white bg-red-500 py-3 px-5 text-md font-medium text-white transition hover:bg-transparent hover:text-gray-700 focus:outline-none active:text-gray-700">
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,8 +4,8 @@ import { Link as LinkRouter } from 'react-router-dom'
 export default function SignUp() {
   return (
     <div className="img_signUp flex justify-center items-center text-center">
-      <div className="flex flex-wrap-reverse rounded-lg bg-white max-w-[80vw] md:max-w-[70vw] lg:max-w-[58vw] md:min-h-[400px] md:max-h-[400px] mt-32 md:mt-56 mb-10 form-shadow">
-        <div className="bg-red-500 md:w-1/2 w-full flex flex-col justify-center items-center p-5 rounded-b-lg md:rounded-br-none md:rounded-tl-lg md:rounded-bl-lg">
+      <div className="border-gray-200 border-2 flex flex-wrap-reverse rounded-lg bg-white max-w-[80vw] md:max-w-[70vw] lg:max-w-[65vw] md:min-h-[400px] mt-32 md:mt-24 mb-10 form-shadow">
+        <div className="bg-red-500 text-white md:w-1/2 w-full flex flex-col justify-center items-center p-5 rounded-b-lg md:rounded-br-none md:rounded-tl-lg md:rounded-bl-lg">
           <h2 className="text-center text-2xl font-bold sm:text-3xl">Hello, Friend!</h2>
           <p className="my-8">Please log in with your personal info</p>
           <button type="submit" className="rounded-3xl border-2 border-white bg-red-500 py-3 px-5 text-md md:font-medium text-white transition hover:bg-transparent hover:text-gray-700 focus:outline-none active:text-gray-700">
