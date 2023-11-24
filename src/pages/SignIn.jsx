07fade4cd78ba5726590f04393c9c0b956b09322
Valentina-Assef <valenassef@gmail.com>
2023-11-24 01:10:13 -0300

    Update SignUp Form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -8,24 +8,13 @@ export default function SignIn() {
         <div className="bg-white md:w-1/2 w-full px-3 md:px-5 rounded-lg">
           <FormSignIn />
         </div>
-        <div className="bg-red-500 md:w-1/2 w-full flex flex-col justify-center items-center p-5 rounded-b-lg md:rounded-bl-none md:rounded-tr-lg md:rounded-br-lg ">
+        <div className="bg-red-500 md:w-1/2 w-full flex flex-col justify-center items-center p-5 rounded-b-lg md:rounded-bl-none md:rounded-tr-lg md:rounded-br-lg">
           <h2 className="text-center text-2xl font-bold sm:text-3xl">No account?</h2>
           <p className="my-8">Enter Your personal details and start journey with us</p>
           <button type="submit" className="rounded-3xl border-2 border-white bg-red-500 py-3 px-5 text-md font-medium text-white transition hover:bg-transparent hover:text-gray-700 focus:outline-none active:text-gray-700">
             <LinkRouter to="/signup" className="">Sign up</LinkRouter>
           </button>
         </div>
-        {/* Contenedor SignIn */}
-        {/* <div className="md:w-1/2 md:px-2 rounded-tl-lg rounded-bl-lg">
-          <FormSignIn />
-        </div>
-        <div className="text-white md:w-1/2 px-5 py-10 bg-red-500 flex flex-col justify-center items-center rounded-tr-lg rounded-br-lg" >
-          <h2 className="text-center text-2xl font-bold sm:text-3xl">No account?</h2>
-          <p className="my-8">Enter Your personal details and start journey with us</p>
-          <button type="submit" className="rounded-3xl border-2 border-white bg-red-500 py-3 px-5 text-md font-medium text-white transition hover:bg-transparent hover:text-gray-700 focus:outline-none active:text-gray-700">
-            <LinkRouter to="/signup" className="">Sign up</LinkRouter>
-          </button>
-        </div> */}
       </div>
     </div>
   )
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,23 +1,21 @@
-import imgSignUp from "/bg_signup.jpg"
 import FormSignUp from "../components/FormSignUp"
 import { Link as LinkRouter } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <section className="flex justify-center items-center text-center">
-      <img alt="Sign In" src={imgSignUp} className="h-[98vh] w-screen object-cover"/>
-      <div className="absolute flex flex-wrap-reverse md:flex-nowrap bg-white rounded-lg w-[60vw] max-w-[90vw] h-[75vh] mt-20 form-shadow">
-        <div className="text-white md:w-1/2 p-5 form-color bg-red-500 flex flex-col justify-center items-center rounded-tl-lg rounded-bl-lg" >
+    <div className="img_signUp flex justify-center items-center text-center">
+      <div className="flex flex-wrap-reverse rounded-lg bg-white max-w-[80vw] md:max-w-[70vw] lg:max-w-[58vw] md:min-h-[400px] md:max-h-[400px] mt-32 md:mt-56 mb-10 form-shadow">
+        <div className="bg-red-500 md:w-1/2 w-full flex flex-col justify-center items-center p-5 rounded-b-lg md:rounded-br-none md:rounded-tl-lg md:rounded-bl-lg">
           <h2 className="text-center text-2xl font-bold sm:text-3xl">Hello, Friend!</h2>
           <p className="my-8">Please log in with your personal info</p>
           <button type="submit" className="rounded-3xl border-2 border-white bg-red-500 py-3 px-5 text-md md:font-medium text-white transition hover:bg-transparent hover:text-gray-700 focus:outline-none active:text-gray-700">
             <LinkRouter to="/signin" className="">Sign in</LinkRouter>
           </button>
         </div>
-        <div className="md:w-1/2 px-2 rounded-tl-lg rounded-bl-lg">
+        <div className="bg-white md:w-1/2 w-full px-4 md:py-4 rounded-lg md:rounded-tl-none">
           <FormSignUp />
         </div>
       </div>
-  </section>
+    </div>
   )
 }
